d8cdb9daf9f1c02272e2a95f7c884c68f6208335
Taoufik <taoufikjarmouni@gmail.com>
2020-10-31 14:52:26 +0100

    Projet Editeur de texte

---
+++ release/moc_memento.cpp
@@ -0,0 +1,194 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'memento.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.14.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../programme_memento/memento.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'memento.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.14.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_Memento_t {
+    QByteArrayData data[18];
+    char stringdata0[489];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_Memento_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_Memento_t qt_meta_stringdata_Memento = {
+    {
+QT_MOC_LITERAL(0, 0, 7), // "Memento"
+QT_MOC_LITERAL(1, 8, 33), // "on_actionAligner_gauche_trigg..."
+QT_MOC_LITERAL(2, 42, 0), // ""
+QT_MOC_LITERAL(3, 43, 33), // "on_actionAligner_droite_trigg..."
+QT_MOC_LITERAL(4, 77, 26), // "on_actionCentrer_triggered"
+QT_MOC_LITERAL(5, 104, 28), // "on_actionJustifier_triggered"
+QT_MOC_LITERAL(6, 133, 26), // "on_actionNouveau_triggered"
+QT_MOC_LITERAL(7, 160, 23), // "on_actionGras_triggered"
+QT_MOC_LITERAL(8, 184, 25), // "on_actionNormal_triggered"
+QT_MOC_LITERAL(9, 210, 27), // "on_actionItalique_triggered"
+QT_MOC_LITERAL(10, 238, 28), // "on_actionSouligner_triggered"
+QT_MOC_LITERAL(11, 267, 35), // "on_actionEnregistrer_sous_tri..."
+QT_MOC_LITERAL(12, 303, 25), // "on_actionOuvrir_triggered"
+QT_MOC_LITERAL(13, 329, 24), // "on_actionAutre_triggered"
+QT_MOC_LITERAL(14, 354, 38), // "on_actionA_propos_de_Memento_..."
+QT_MOC_LITERAL(15, 393, 30), // "on_actionEnregistrer_triggered"
+QT_MOC_LITERAL(16, 424, 36), // "on_actionInserer_une_image_tr..."
+QT_MOC_LITERAL(17, 461, 27) // "on_actionImprimer_triggered"
+
+    },
+    "Memento\0on_actionAligner_gauche_triggered\0"
+    "\0on_actionAligner_droite_triggered\0"
+    "on_actionCentrer_triggered\0"
+    "on_actionJustifier_triggered\0"
+    "on_actionNouveau_triggered\0"
+    "on_actionGras_triggered\0"
+    "on_actionNormal_triggered\0"
+    "on_actionItalique_triggered\0"
+    "on_actionSouligner_triggered\0"
+    "on_actionEnregistrer_sous_triggered\0"
+    "on_actionOuvrir_triggered\0"
+    "on_actionAutre_triggered\0"
+    "on_actionA_propos_de_Memento_triggered\0"
+    "on_actionEnregistrer_triggered\0"
+    "on_actionInserer_une_image_triggered\0"
+    "on_actionImprimer_triggered"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_Memento[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      16,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   94,    2, 0x08 /* Private */,
+       3,    0,   95,    2, 0x08 /* Private */,
+       4,    0,   96,    2, 0x08 /* Private */,
+       5,    0,   97,    2, 0x08 /* Private */,
+       6,    0,   98,    2, 0x08 /* Private */,
+       7,    0,   99,    2, 0x08 /* Private */,
+       8,    0,  100,    2, 0x08 /* Private */,
+       9,    0,  101,    2, 0x08 /* Private */,
+      10,    0,  102,    2, 0x08 /* Private */,
+      11,    0,  103,    2, 0x08 /* Private */,
+      12,    0,  104,    2, 0x08 /* Private */,
+      13,    0,  105,    2, 0x08 /* Private */,
+      14,    0,  106,    2, 0x08 /* Private */,
+      15,    0,  107,    2, 0x08 /* Private */,
+      16,    0,  108,    2, 0x08 /* Private */,
+      17,    0,  109,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void Memento::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<Memento *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->on_actionAligner_gauche_triggered(); break;
+        case 1: _t->on_actionAligner_droite_triggered(); break;
+        case 2: _t->on_actionCentrer_triggered(); break;
+        case 3: _t->on_actionJustifier_triggered(); break;
+        case 4: _t->on_actionNouveau_triggered(); break;
+        case 5: _t->on_actionGras_triggered(); break;
+        case 6: _t->on_actionNormal_triggered(); break;
+        case 7: _t->on_actionItalique_triggered(); break;
+        case 8: _t->on_actionSouligner_triggered(); break;
+        case 9: _t->on_actionEnregistrer_sous_triggered(); break;
+        case 10: _t->on_actionOuvrir_triggered(); break;
+        case 11: _t->on_actionAutre_triggered(); break;
+        case 12: _t->on_actionA_propos_de_Memento_triggered(); break;
+        case 13: _t->on_actionEnregistrer_triggered(); break;
+        case 14: _t->on_actionInserer_une_image_triggered(); break;
+        case 15: _t->on_actionImprimer_triggered(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+QT_INIT_METAOBJECT const QMetaObject Memento::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_Memento.data,
+    qt_meta_data_Memento,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *Memento::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *Memento::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_Memento.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int Memento::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 16)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 16;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 16)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 16;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
